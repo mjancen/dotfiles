7d6c5ecf83edf02574fc40256abea6066e06d6d7
Maksim Jancenko <maksimjancenko@gmail.com>
2025-03-08 21:12:58 +0000

    Updated post-install.sh

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # mak-config
-Config files that I use. NOTE: the dotfiles repo should be cloned into the $HOME directory of the filesystem.
+Config files that I use, and other useful stuff.
+
+NOTE: the dotfiles repo should be cloned into the $HOME directory of the filesystem.
 
--- post-install.sh
+++ post-install.sh
@@ -14,37 +14,66 @@ set +x
 # basic update
 sudo apt-get -y --force-yes update
 sudo apt-get -y --force-yes upgrade
+echo Updated and upgraded.
 
 # install apps
-apt-get install \
-	git curl gparted vim yakuake gcc
+for app in git curl gparted vim neovim yakuake gcc steam-installer; do
+	echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	echo Installing $app
+	apt install $app
+done
 
 # VS Code
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo Installing VS Code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 apt-get install apt-transport-https
 apt-get update
 apt-get install code # or code-insiders
+echo VS Code installed.
 
 # Miniconda
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo Installing Miniconda
 curl -O https://repo.continuum.io/miniconda/Miniconda3-4.5.12-Linux-x86-64.sh
 bash Miniconda3-4.5.12-Linux-x86-64.sh
+echo Miniconda installed.
 
 # Flatpak
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo Installing flatpak
 apt install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo flatpak installed.
 
 # Zotero
 # flatpak install -y flathub org.zotero.Zotero
 
 # Spotify
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo Installing Spotify
 flatpak install -y flathub com.spotify.Client
+echo Spotify installed.
 
+# Steam
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo Installing steam-installer
+apt-get install steam-installer
+echo steam-installer installed. Run steam-installer to install Steam.
+
+# Vivaldi
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo Installing Vivaldi
+curl -O https://downloads.vivaldi.com/stable/vivaldi-stable_7.1.3570.58-1_amd64.deb
+
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ./create-config-links.sh
+echo Config file links created.
 
-
-#echo "Post-installation process finished."
-#echo "Time for a reboot!"
+echo %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+echo "Post-installation process finished."
+echo "Time for a reboot!"
 
 
